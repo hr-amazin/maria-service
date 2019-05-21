e77141a1091eecbd511219da416ee93c72691945
Maria Jose <mariajosenc89@gmail.com>
2019-05-21 16:40:26 -0500

    Setting up enviroment variables, connecting database, dockerfile and dockerun file are created to deployed.

--- server/db.js
+++ server/db.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/amazin');
-// mongodb://127.0.0.1:27017/amazin'
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_URL = process.env.MONGO_URL;
+
+console.log('hola', MONGO_URL);
+
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_URL}`, { useNewUrlParser: true });
+//ENVIROMENT VARIABLE:
+// mongoose.connect(`mongodb+srv://USERNAME:PASSWORDo@cluster0-x46xc.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true });
+//'mongodb://localhost/amazin'
+//my desployment: http://description.us-east-2.elasticbeanstalk.com/
+
 
 // let schema = mongoose.Schema({
 //   _id: Number,
@@ -19,14 +29,18 @@ const connect = mongoose.connection;
 
 module.exports.getOne = (callback) => {
   connect.db.collection('descriptions', (err, collection) => {
-    collection.findOne(function(err, result) {
-      if(err) {
-        callback(err);
-      } else {
-        console.log('this is result DB ', result);
-        callback(null, result);
-      }
-    });
+    if(err) {
+      console.log('there is an error', err);
+    } else {
+      collection.findOne(function(err, result) {
+        if(err) {
+          callback(err);
+        } else {
+          console.log('this is result DB ', result);
+          callback(null, result);
+        }
+      });
+    }  
   });
 }
 
