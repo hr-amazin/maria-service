104ebad8ac321f32928257af736c20c7b96e6f14
Maria Jose <mariajosenc89@gmail.com>
2019-05-24 17:53:30 -0500

    Price component logic is modified and image version in dockerrun.aws.json is modified

--- client/src/components/product-price/price.jsx
+++ client/src/components/product-price/price.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from '../product-price/price.module.css';
 
 const Price = ({price}) =>  {
-  let percentages = [.08, .10, .15, .20];
+  let percentages = [8, 10, 15, 20];
   let randomIndex = Math.floor(Math.random() * percentages.length);
   let randomPercentage = percentages[randomIndex];
-  let listPrice = Math.floor(price * (1 + randomPercentage));
+  let listPrice = Math.floor((price * 100) /( 100 - randomPercentage));
   listPrice = listPrice.toFixed(2);
   let save = Math.floor(listPrice - price);
   save = save.toFixed(2);
@@ -32,7 +32,7 @@ const Price = ({price}) =>  {
           </tr>
           <tr>
             <td className={styles.category}>You save:</td>
-            <td className={styles.savePrice}>${save} ({randomPercentage * 100}%)</td>
+            <td className={styles.savePrice}>${save} ({randomPercentage}%)</td>
           </tr>
         </tbody>
       </table>
